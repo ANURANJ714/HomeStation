cac9f807ae432722f225a153ee608d8e84c7fe6a
ANURANJ714 <anuranjsreemandir@gmail.com>
2026-08-14 11:14:37 +0530

    Removed unwanted code snippets in error handling

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -1,20 +1,9 @@
 import User from '../models/User.js';
 
-// export const verifyAdmin = (req, res, next) => {
-//     if (req.isAuthenticated() && req.user && req.user.role === 'Admin') {
-//         return next(); 
-//     }
-    
-//     res.redirect('/admin/login');
-// };
-
 export const verifyAdmin = (req, res, next) => {
-    console.log(`[DEBUG verifyAdmin] Intercepted path: ${req.originalUrl}`);
-    
     if (req.isAuthenticated() && req.user && req.user.role === 'Admin') {
         return next(); 
     }
     
-    console.log(`[DEBUG verifyAdmin] Redirecting unauthenticated user from: ${req.originalUrl}`);
     res.redirect('/admin/login');
 };
